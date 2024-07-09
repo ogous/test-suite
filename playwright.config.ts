1a971b2b7f81a1fe01ce3c662c6e2a70354665e4
ogous <oguzxan.ali@gmail.com>
2024-07-09 18:43:49 +0300

    enable outdir

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
   // If a test fails, retry it additional 2 times
   retries: 2,
   // Artifacts folder where screenshots, videos, and traces are stored.
-  // outputDir: "./test-results",
+  outputDir: "./test-results",
 
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
